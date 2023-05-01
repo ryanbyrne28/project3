389136180601c2047aa0d6c549bbab2b8b50faf1
brendanmac001 <atlasmaccc@gmail.com>
2023-05-01 16:15:01 -0400

    adding songs

--- music_player/main_app/models.py
+++ music_player/main_app/models.py
@@ -17,11 +17,17 @@ class Playlist(models.Model):
 class Song(models.Model):
 	title=models.CharField()
 	artist=models.CharField()
-	image=models.ImageField()
+	image=models.ImageField(blank=True,null=True)
 	audio_file=models.FileField(blank=True,null=True)
 	audio_link=models.CharField(max_length=150,blank=True,null=True)
-	duration=models.CharField(max_length=20)
+	duration=models.CharField(max_length=20,blank=True,null=True)
 	paginate_by=2
+
+	def __str__(self):
+		return f"{self.title}"
+	
+	def get_absolute_url(self):
+		return reverse('songs_detail', kwargs={'pk': self.id})
 
 
 	
--- music_player/main_app/urls.py
+++ music_player/main_app/urls.py
@@ -10,4 +10,13 @@ urlpatterns = [
     path('playlists/create/', views.PlaylistCreate.as_view(), name='playlist_create'),
     path('playlists/<int:pk>/update/', views.PlaylistUpdate.as_view(), name='playlist_update'),
     path('playlists/<int:pk>/delete/', views.PlaylistDelete.as_view(), name='playlist_delete'),
+
+    path('playlists/<int:playlist_id>/assoc_song/<int:song_id>/', views.assoc_song, name='assoc_song'),
+    path('playlists/<int:playlist_id>/remove_song/<int:song_id>/', views.remove_song, name='remove_song'),
+    
+    path('songs/create/', views.SongCreate.as_view(), name='songs_create'),
+    path('songs/', views.SongsIndex.as_view(), name='songs_index'),
+    path('songs/<int:pk>/', views.SongsDetail.as_view(), name='songs_detail'),
+    path('songs/<int:pk>/update/', views.SongUpdate.as_view(), name='songs_update'),
+    path('songs/<int:pk>/delete/', views.SongDelete.as_view(), name='songs_delete'),
 ]
